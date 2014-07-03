71a473a2e4bc20016cc1e466efafe2fac0ae7ee1
Raymond Ho <raymond@4mation.com.au>
2014-07-03 14:36:42 +1000

    issue with structure

--- Module.php
+++ Module.php
@@ -25,7 +25,7 @@ class Module implements ConfigProviderInterface
     {
         return array('Zend\Loader\StandardAutoloader' => array(
             'namespaces' => array(
-                __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                __NAMESPACE__ => __DIR__ . '/src/Analytics',
             ),
         ));
     }
